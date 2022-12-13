5e990e9242559aeb275553cb0a59f2fca2c5d522
Martín <magadiflo@gmail.com>
2022-12-13 10:48:07 -0500

    Herencia - Problemática

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,13 +1,51 @@
 //* (() => { ...código.... })(); Función anónima autoinvocada para que el código quede aislado.
 (() => {
 
+    //* No aplicando el Principio de Responsabilidad Única
     type Gender = 'M' | 'F';
 
     class Person {
-        constructor(public name: string, public gender: Gender, public birthday: Date) { }
+        constructor(
+            public name: string,
+            public gender: Gender,
+            public birthdate: Date) { }
     }
 
-    const newPerson = new Person('Martín', 'M', new Date('1995-10-20'));
-    console.log({ newPerson });
+    class User extends Person {
+
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings('/user/home', '/home', 'maga@gmail.com', 'Admin', 'Martín', 'M', new Date('1990-10-10'));
+    console.log({ userSettings });
 
 })();   
